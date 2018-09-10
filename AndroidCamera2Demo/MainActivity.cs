9de7739da692d6b4461546fde3dd485e77a485be
Matt Hoffman <hofmadresu@gmail.com>
2018-09-10 15:54:38 -0400

    Set orientation to portrait

--- AndroidCamera2Demo/MainActivity.cs
+++ AndroidCamera2Demo/MainActivity.cs
@@ -4,10 +4,11 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using AndroidCamera2Demo.Controls;
+using Android.Content.PM;
 
 namespace AndroidCamera2Demo
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
